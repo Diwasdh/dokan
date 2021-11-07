e5986d6c0380bda7fe362f23c6cdda69c43e2ed9
diwasdh <diwas@goswivt.com>
2021-11-07 23:56:45 +0545

    --

--- components/include/Footer.js
+++ components/include/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function footer() {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <p>&copy; Copyright Diwas Dhakal</p>
+    </div>
+  );
 }
--- components/include/Header.js
+++ components/include/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 export default function header() {
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
+  return (
+    <div>
+      <nav className="navbar">
+        <Link href='/'>Home</Link>
+        <Link href='/contactus'>Contact Us</Link>
+        <Link href='/aboutus'>About Us</Link>
+      </nav>
+    </div>
+  );
 }
